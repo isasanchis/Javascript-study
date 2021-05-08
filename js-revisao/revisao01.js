fd7a93ab39ef813849c2a77f94f430dcfff7934a
Isabella <isasanchis3@gmail.com>
2021-05-08 15:39:59 -0300

    updates

---
+++ js-revisao/revisao01.js
@@ -0,0 +1,10 @@
+// template string
+
+const nome = 'Maria'
+console.log(`${nome} é legal!`) //Maria é legal!
+
+//destructuring
+
+const [i, s, ...lla] = "Cod3r"
+console.log(i, s, lla) //C o ['d', '3', 'r']
+
